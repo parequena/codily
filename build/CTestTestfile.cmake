93c74486639c761612455ea01f699a7b99660689
Pablo <parequena8@gmail.com>
2022-12-15 13:47:10 +0100

    Primer Commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pablo/codily
-# Build directory: /home/pablo/codily/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
